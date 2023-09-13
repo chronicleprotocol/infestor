04c2915043635d74239f1d9b309f61d490e104e4
Hungry Warrior <hungrywarrior081@gmail.com>
2023-09-13 16:38:44 -0400

    Fix lint error

--- e2e/exchanges_test.go
+++ e2e/exchanges_test.go
@@ -414,7 +414,7 @@ func (s *ExchangesE2ESuite) TestCurve() {
 			//	Address: types.MustAddressFromHex("0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
 			//	Args:   []any{},
 			//	Return: []any{"ETH"},
-			//},
+			// },
 			{
 				Address: types.MustAddressFromHex("0xae7ab96520de3a18e5e111b5eaab095312d7fe84"),
 				Args:    []any{},
@@ -426,7 +426,7 @@ func (s *ExchangesE2ESuite) TestCurve() {
 			//	Address: types.MustAddressFromHex("0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
 			//	Args:   []any{},
 			//	Return: []any{big.NewInt(18)},
-			//},
+			// },
 			{
 				Address: types.MustAddressFromHex("0xae7ab96520de3a18e5e111b5eaab095312d7fe84"),
 				Args:    []any{},
